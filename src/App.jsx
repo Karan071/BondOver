f7eb6af3d14e4cd4104bf3f32832f6bb54096c45
Karan <kc2532002@gmail.com>
2025-06-09 15:12:25 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./page/Landing.jsx";
 import PartnerInterest from "./page/PartnerInterest/PartnerInterest.jsx";
 import Sponser from "./page/Sponsor/Sponser.jsx";
+import Register from "./page/Register/Register.jsx";
+import Join from "./page/Join/Join.jsx";
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/partner" element={<PartnerInterest/>}/>
       <Route path="/sponser" element={<Sponser/>} />
-      {/* <Route path="/register" element={} /> */}
+      <Route path="/register" element={<Register/>} />
+      <Route path="/join" element={<Join/>} />
       
     </Routes>
     </>
